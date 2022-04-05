9694f46811b9d02c3e24527f50512250c37d04b8
Ubuntu <ubuntu@ip-172-31-79-182.ec2.internal>
2022-04-05 01:20:34 +0000

    program says month with user input

---
+++ month.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Created By: Titwech Wal
+# Date: April.4. 2022
+
+# program tells the user what month
+# it is based on the number input
+
+
+def find_month(month):
+    months = {
+        1: "{} represents January.". format(month),
+        2: "{} represents February.". format(month),
+        3: "{} represents March.". format(month),
+        4: "{} represents April.". format(month),
+        5: "{} represents May.". format(month),
+        6: "{} represents June.". format(month),
+        7: "{} represents July.". format(month),
+        8: "{} represents August.". format(month),
+        9: "{} represents September.". format(month),
+        10: "{} represents October.". format(month),
+        11: "{} represents November.". format(month),
+        12: "{} represents December.". format(month),
+    }
+
+    return months.get(month, "Error. {} does not represent a month.".
+                      format(month))
+
+
+if __name__ == "__main__":
+    user_month = int(input("Enter the number for a month"
+                           "(i.e 1 for January) : "))
+print(find_month(user_month))
